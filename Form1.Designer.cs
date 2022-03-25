31dfbc2a9bc80771a3a890a2ae7b8c34ff286a8d
projectai364 <101421870+projectai364@users.noreply.github.com>
2022-03-25 15:03:01 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.G = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // G
+            // 
+            this.G.BackColor = System.Drawing.Color.Gray;
+            this.G.Location = new System.Drawing.Point(10, 10);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(565, 588);
+            this.G.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 613);
+            this.Controls.Add(this.G);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "checkers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel G;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n;
+        PictureBox[,] P;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = 8;
+            P = new PictureBox[n, n];
+            int left = 2, top = 2;
+            Color[] colors = new Color[] { Color.White, Color.Black };
+            for (int i = 0; i < n; i++)
+            {
+                left = 2;
+                if (i % 2 == 0) { colors[0] = Color.White; colors[1] = Color.Black; }
+                else { colors[0] = Color.Black; colors[1] = Color.White; }
+
+                for (int j = 0; j < n; j++)
+                {
+                    P[i, j] = new PictureBox();
+                    P[i, j].BackColor = colors[(j % 2 == 0) ? 1 : 0];
+                    P[i, j].Location = new Point(left, top);
+                    P[i, j].Size = new Size(60, 60);
+                    left += 60;
+                    P[i, j].Name = i + "" + j;
+                    if(i<(n/2)-1 && P[i,j].BackColor == Color.Black) { P[i, j].Image = Properties.Resources.rr;P[i, j].Name += "r"; }
+                    else if(i>(n/2) && P[i,j].BackColor == Color.Black)
+                    {
+                        P[i, j].Image = Properties.Resources.gg; P[i, j].Name += "g";
+                    }
+                    P[i, j].SizeMode = PictureBoxSizeMode.CenterImage;
+                    P[i, j].MouseHover += (sender2, e2) =>
+                     {
+                         PictureBox p = sender2 as PictureBox;
+                         if (p.Image != null) p.BackColor = Color.FromArgb(255, 64, 64, 64);
+                     };
+                    P[i, j].MouseHover += (sender2, e2) =>
+                    {
+                        PictureBox p = sender2 as PictureBox;
+                        if (p.Image != null) p.BackColor = Color.Black;
+                    };
+
+                    G.Controls.Add(P[i, j]);
+                }
+                top += 60;
+            }
+        }
+    }
+}
